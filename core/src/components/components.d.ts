6d28bcd9a7ad133bb4599eae3e179e97306e8fb6
Gustavo Garzaki <garzaki@pm.me>
2020-05-24 09:14:41 -0500

    move to src/components dir

--- core/src/components/components.d.ts
+++ core/src/components/components.d.ts
@@ -5,7 +5,7 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { IAnimationHandler, } from "./components/storify/types";
+import { IAnimationHandler, } from "./types";
 export namespace Components {
     interface XStorify {
         "duration": number;
